ce3ca37dc161942e36441c93283c3e9828e4857e
pranavp0 <67086150+pranavp0@users.noreply.github.com>
2022-02-22 15:14:57 +0530

    added python files

---
+++ Task-4/program-2.py
@@ -0,0 +1,5 @@
+a='Lorem Ipsum'                         # input
+b=''
+for i in range(0,len(a),2):             # looping will give even index
+    b=b+a[i]                            # catenation
+print(b)    
