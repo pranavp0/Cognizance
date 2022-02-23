5c72d2437fe0dbf9e91b2447d5bd13d98694c704
pranavp0 <67086150+pranavp0@users.noreply.github.com>
2022-02-23 09:45:56 +0530

    Delete program-3.py

--- Task-4/program-3.py
+++
@@ -1,9 +0,0 @@
-a=[[1,'Abc',90],[2,'Def',95],[3,'Ghi',85]]          # input list
-b=['Rollno','Name','Marks']                         # input list column name
-for i in range(0,len(b)):                           # printing column names
-    print(b[i],end='   ')
-print()    
-for j in range(0,len(a)):                           # printing rows
-    print()
-    for i in range(0,len(a)):
-        print(a[j][i],end='       ')
